724f969ee3d5fcd5067dcb5c98f0a58a228acfa7
mcchukwu <miraclechukwu2011@gmail.com>
2025-02-25 00:40:55 +0100

    hero text animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
 import "../css/base.css";
 import "../css/index.css";
+
+import { gsap } from 'gsap';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tl = gsap.timeline();
+
+  tl.to('.hero-text h1', { opacity: 1, y: 0, duration: 1, ease: 'power2.out' })
+    .to('.hero-text p', { opacity: 1, y: 0, duration: 1, ease: 'power2.out' }, '-=0.5')
+    .to('.hero-text a', { opacity: 1, y: 0, duration: 1, ease: 'power2.out' }, '-=0.5');
+});
